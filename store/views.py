8ac1ae53d4777625f0a7a951d5897bd53b494ae7
GloireEBERE <gloireebre@gmail.com>
2024-04-09 00:10:55 +0000

    commit

--- store/views.py
+++ store/views.py
@@ -14,10 +14,14 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
-        paginator = Paginator(products, 6)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        # Le nombre d'élément à afficher
+        paginator = Paginator(products, 1)
         page = request.GET.get('page')
         paged_product = paginator.get_page(page)
         product_count = products.count()
