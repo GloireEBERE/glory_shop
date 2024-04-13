b3fe5a40b1bf5bc1822df0c5dfb6cc1fee7a2f53
GloireEBERE <gloireebre@gmail.com>
2024-04-13 11:46:02 +0000

    Django authentification

---
+++ accounts/forms.py
@@ -0,0 +1,35 @@
+from typing import Any
+from django import forms
+from .models import Account
+
+class Registrationform(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Saisissez votre mot de passe',
+        'class': 'form-control',
+    }))
+    
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirmez votre mot de passe'
+    }))
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+    
+    def __init__(self, *args, **kwargs):
+        super(Registrationform, self).__init__(*args, *kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+    
+    def clean(self):
+        cleaned_data = super(Registrationform, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Le mot de passe est incorrect."
+            )
